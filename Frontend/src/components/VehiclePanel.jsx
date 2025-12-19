86597160db35278ee22c638d3b85999dfec6f298
Nishant chauhan <nishantchauhannn@gmail.com>
2025-12-19 21:02:06 +0530

    Update vehicle images in VehiclePanel component

--- Frontend/src/components/VehiclePanel.jsx
+++ Frontend/src/components/VehiclePanel.jsx
@@ -24,7 +24,9 @@ const VehiclePanel = (props) => {
                 props.setConfirmRidePanel(true)
                 props.selectVehicle('moto')
             }} className='flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-centre'>
-                <img className='h-10' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_638,w_956/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png" alt="" />
+              
+                <img className='h-10' src="https://upload.wikimedia.org/wikipedia/commons/2/22/Motorbike.jpg" alt="" />
+                <img className='h-10' src=''></img>
                 <div className='ml-2 w-2/4'>
                     <h4 className='font-medium text-base'>Moto <span><i className="ri-user-3-fill"></i>1</span></h4>
                     <h5 className='font-medium text-sm'>3 mins away </h5>
@@ -36,7 +38,7 @@ const VehiclePanel = (props) => {
                 props.setConfirmRidePanel(true)
                 props.selectVehicle('auto')
             }} className='flex border-2 active:border-black mb-2 rounded-xl w-full p-3  items-center justify-centre'>
-                <img className='h-10' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png" alt="" />
+                <img className='h-10' src="https://upload.wikimedia.org/wikipedia/commons/5/52/Auto.jpeg" alt="" />
                 <div className='ml-2 w-1/2'>
                     <h4 className='font-medium text-base'>UberAuto <span><i className="ri-user-3-fill"></i>3</span></h4>
                     <h5 className='font-medium text-sm'>3 mins away </h5>
