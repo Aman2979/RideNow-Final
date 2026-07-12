02f7d3b19c5a2bd01e815d740709ecf5637b3214
Aman Kumar <ak93896897@gmail.com>
2026-07-12 16:32:54 +0530

    Change in packeges

--- Backend/app.js
+++ Backend/app.js
@@ -22,7 +22,7 @@ const corsOptions = {
   origin: [
     "https://ride-now-final-5ipw.vercel.app",
     "http://localhost:5173",
-    // "http://localhost:5174",
+    "http://localhost:5174",
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
